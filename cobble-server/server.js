951a4d3d19a71e70c1fffb58b73b6642dd0b469a
ConanHUN <conanhun@hotmail.com>
2026-04-29 09:52:02 +0200

    refactor: remove Aikar's GC flags from Minecraft server launch arguments

--- cobble-server/server.js
+++ cobble-server/server.js
@@ -102,12 +102,6 @@ function startMinecraft() {
   console.log("[Minecraft] Szerver indítása (Aikar's flags, 8GB RAM)...")
   const javaArgs = [
     '-Xmx8G', '-Xms2G',
-    '-XX:+UseG1GC', '-XX:+ParallelRefProcEnabled', '-XX:MaxGCPauseMillis=200',
-    '-XX:+UnlockExperimentalVMOptions', '-XX:+DisableExplicitGC', '-XX:G1NewSizePercent=30',
-    '-XX:G1MaxNewSizePercent=40', '-XX:G1HeapRegionSize=8M', '-XX:G1ReservePercent=20',
-    '-XX:G1HeapWastePercent=5', '-XX:G1MixedGCCountTarget=4', '-XX:InitiatingHeapOccupancyPercent=15',
-    '-XX:G1MixedGCLiveThresholdPercent=90', '-XX:G1RSetUpdatingPauseTimePercent=5',
-    '-XX:SurvivorRatio=32', '-XX:+PerfDisableSharedMem', '-XX:MaxTenuringThreshold=1',
     '-jar', 'fabric-server-launch.jar', 'nogui'
   ]
   mcProcess = spawn(activeJavaPath, javaArgs, {
