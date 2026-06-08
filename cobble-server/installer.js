8a84af1789a1c07c7b2e0ab83aeb52bd8c66fd77
ConanHUN <conanhun@hotmail.com>
2026-06-08 12:29:25 +0200

    feat: remove 'modern-ui' from EXTRA_MODS list for compatibility

--- cobble-server/installer.js
+++ cobble-server/installer.js
@@ -529,7 +529,6 @@ const EXTRA_MODS = [
   { slug: 'cobblemon-tents',                loaders: ['fabric'], gameVersions: [MC_VERSION] },
   { slug: 'cobblemon-snap',                 loaders: ['fabric'], gameVersions: [MC_VERSION] },
   { slug: 'cobblemon-villager-overhaul',    loaders: ['fabric'], gameVersions: [MC_VERSION] },
-  { slug: 'modern-ui',                    loaders: ['fabric'], gameVersions: [MC_VERSION] },
   { slug: 'tt20',                         loaders: ['fabric'], gameVersions: [MC_VERSION] },
   // Függőségek
   { slug: 'cobblemore-library',             loaders: ['fabric'], gameVersions: [MC_VERSION] },
